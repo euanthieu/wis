451c478d82d15a8b7ca2703ddf9ffa5df063bf91
euanthieu <euanthieu@gmail.com>
2023-10-06 12:15:43 +0800

    push 03

---
+++ 03_php_syntax_overview.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>03_php_syntax_overview</title>
+</head>
+<body>
+    <?php 
+        $capital = 67;
+        print("Variable capital is $capital<br>");
+        print("Variable CaPiTaL is $CaPiTaL<br>");
+
+        echo "<br>";
+        echo "Hello PHP!!!!!";
+
+    ?>
+</body>
+</html>
